c2561cae770c68c1c56f82290c1235544bc706c6
James Mobus <mobusj@live.com>
2020-05-22 18:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function writeCards(arrayOfNames) {
   let ret = []
   
   for (let i = 0; i < arrayOfNames.length; i++) {
-   ret.append("Thank you, ${arrayOfNames[i]}, for the wonderful birthday gift!")
+   ret.push("Thank you, ${arrayOfNames[i]}, for the wonderful birthday gift!")
   }
   
   return ret
