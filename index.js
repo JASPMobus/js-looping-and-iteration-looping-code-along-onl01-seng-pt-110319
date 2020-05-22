b2b86a05849a02009b4cfccfcbab096a7e025195
James Mobus <mobusj@live.com>
2020-05-22 18:26:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames) {
+  let ret = []
+  
+  for (let i = 0; i < arrayOfNames.length; i++) {
+   ret.push(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!`)
+  }
+  
+  return ret
+}
+
+function countDown(fromThis) {
+  while (fromThis >= 0) {
+    console.log(fromThis)
+    
+    fromThis--
+  }
+}
