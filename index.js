a55d2517b8c20116dc54c5482f7d25b9af52aabe
James Mobus <mobusj@live.com>
2020-05-22 18:19:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(arrayOfNames) {
-  for (let i = 0, i < arrayOfNames.length; i++) {
+  for (let i = 0; i < arrayOfNames.length; i++) {
     console.log("Thank you, ${arrayOfNames[i]}, for the wonderful birthday gift!")
   }
 }
